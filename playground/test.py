faefaf223eee8a20e84cdc7fcd6e600c5d3267c4
Marius <whalter123a@gmail.com>
2026-09-16 10:54:40 +0200

    Implemented option to return statevector

--- playground/test.py
+++ playground/test.py
@@ -3,9 +3,10 @@ import block_quantenschaltung as qs
 circ = qs.qiskit.QuantumCircuit(2)
 circ.h(0)
 circ.cx(0, 1)
+circ.save_statevector()
 circ.measure_all()
 
-test = qs.simulate(circ, 1000)
+test = qs.simulate(circ, 1000, False)
 res = test.perform_sim()
 
-print(res.get_counts(circ))
+print(res)
--- src/block_quantenschaltung/__init__.py
+++ src/block_quantenschaltung/__init__.py
@@ -4,13 +4,16 @@ from qiskit_aer import AerSimulator
 
 
 class simulate:
-    def __init__(self, circuit: qiskit.QuantumCircuit, number_of_shots: int):
+    def __init__(self, circuit: qiskit.QuantumCircuit, number_of_shots: int, return_statevector: bool):
         self.circuit = circuit
         self.number_of_shots = number_of_shots
+        self.return_statevector = return_statevector
     def perform_sim(self):
-        simulator = AerSimulator()
+        simulator = AerSimulator(method="statevector")
         result = simulator.run(self.circuit, shots = self.number_of_shots).result()
-        return result
+        if self.return_statevector:
+            return result.get_statevector(self.circuit)
+        return result.get_counts(self.circuit)
     def own_simulator(self):
         return 0
     
