c30f516efa4da9f0bb444cce73954d47d23f5467
Pascal <st183508@stud.uni-stuttgart.de>
2026-09-17 11:08:24 +0200

    keine ahnung diggi

--- docs/conf.py
+++ docs/conf.py
@@ -13,7 +13,7 @@ author = 'Pascal and Marius'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = ['sphinx.ext.autosummary's]
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
